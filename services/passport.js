a55d10951fa38b20802a59bff36da68e5b76ef59
dyrewolf <stryder6044@gmail.com>
2018-05-16 21:31:52 -0700

    Google OAuth Working

--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,16 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
 passport.use(
   new GoogleStrategy(
     {
@@ -13,7 +23,17 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      new User({ googleID: profile.id }).save();
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          //we already have a record with the given profile ID
+          done(null, existingUser);
+        } else {
+          //we don't have a user record with this ID,
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );
